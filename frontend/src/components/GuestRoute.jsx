1d6065f8e57702e2b69b2de42ff4de8f90734f58
Amay <agrawalamay1master@gmail.com>
2026-04-16 14:18:14 +0530

    add guest route

---
+++ frontend/src/components/GuestRoute.jsx
@@ -0,0 +1,18 @@
+// src/components/GuestRoute.jsx
+
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const GuestRoute = ({ children }) => {
+  const { user } = useAuth();
+
+  if (user) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
+};
+
+
+export default GuestRoute;
